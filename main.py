6e77d71a9af9dce8e1b20330e9f59a4dc21f944f
trimmjens <48127097+trimmjens@users.noreply.github.com>
2023-11-29 21:12:47 +0100

    Create main.py
    First file created

---
+++ main.py
@@ -0,0 +1,5 @@
+# Garage door trigger with limit switch for open and closed,
+# relay for moving door, PIR sensor, BME280 for temperature, humidity and pressure
+# and IR beam for detecting obstruction of garage door
+
+micropython code...
