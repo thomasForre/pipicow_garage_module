73604937377bb18056497b602927e3052acf7780
Thomas Førre <48127097+thomasForre@users.noreply.github.com>
2024-09-29 22:49:03 +0200

    Changed definitions from snake_case to camelCase
    Asked chatGPT to change all definitions from snake_case to camelCase

--- rewritten main.py
+++ rewritten main.py
@@ -1,5 +1,5 @@
 # Connect Raspberry Pi PicoW to WIFI and MQTT
-# Rewritten original file into a class by chatGPT 29.09.24
+# Main.py rewritten to a class by chatGTP
 
 import sys
 import time
@@ -22,28 +22,28 @@ class RaspberryPiPicoW:
         self.subscriptionTopic = "pipicow"
 
         self.i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
-        self.initialize_pins()
-        self.initialize_flags()
+        self.initializePins()
+        self.initializeFlags()
 
         # Flash LEDs on boot
-        self.blink_led(self.ledInternal, 1, 500)
-        self.blink_led(self.ledAlive, 1, 500)
+        asyncio.run(self.blinkLed(self.ledInternal, 1, 500))
+        asyncio.run(self.blinkLed(self.ledAlive, 1, 500))
 
         try:
             wifiConnect()
         except RuntimeError as err:
-            self.write_to_log(str(err.args[0]) + str(err.args[1]))
+            self.writeToLog(str(err.args[0]) + str(err.args[1]))
             machine.reset()
 
-        self.client = self.mqtt_connect()
+        self.client = self.mqttConnect()
 
         # Set up handlers for GPIO interrupts
-        self.setup_handlers()
+        self.setupHandlers()
 
         # Schedule tasks
-        self.schedule_tasks()
+        self.scheduleTasks()
 
-    def initialize_pins(self):
+    def initializePins(self):
         self.switchDoorOpen = Pin(3, Pin.IN, Pin.PULL_DOWN)
         self.switchDoorClosed = Pin(4, Pin.IN, Pin.PULL_DOWN)
         self.relayDoorMoving = Pin(7, Pin.IN, Pin.PULL_DOWN)
@@ -53,13 +53,13 @@ class RaspberryPiPicoW:
         self.ledAlive = Pin(12, mode=Pin.OUT)
         self.ledInternal = Pin("LED", Pin.OUT)
 
-    def initialize_flags(self):
+    def initializeFlags(self):
         self.doorStateOpen = False
         self.doorStateClosed = False
         self.doorStateMoving = False
         self.doorStateObstructed = False
 
-    async def blink_led(self, led, nTimes, periodMs):
+    async def blinkLed(self, led, nTimes, periodMs):
         blinkSpeed = periodMs / 1000
         for i in range(nTimes):
             led.toggle()
@@ -69,12 +69,12 @@ class RaspberryPiPicoW:
                 return
             await asyncio.sleep(blinkSpeed)
 
-    def write_to_log(self, logString):
+    def writeToLog(self, logString):
         with open("log.txt", "a") as logFile:
-            logFile.write(self.perfect_date_time() + "\n")
+            logFile.write(self.perfectDateTime() + "\n")
             logFile.write("   " + logString + "\n")
 
-    def perfect_date_time(self):
+    def perfectDateTime(self):
         DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         now = time.localtime()
         weekDay = DAYS[now[6]]
@@ -89,7 +89,7 @@ class RaspberryPiPicoW:
         dateString = day + "." + month + "." + year
         return weekDay + " " + dateString + " @ " + timeString
 
-    def mqtt_subscription_callback(self, topic, message):
+    def mqttSubscriptionCallback(self, topic, message):
         if "OTA" in message:
             self.client.publish("pipicow/info", "Update command received...")
             if ota_updater.download_and_install_update_if_available():
@@ -102,45 +102,45 @@ class RaspberryPiPicoW:
             time.sleep(0.5)
             self.relayDoorTrigger.value(1)
 
-    def mqtt_connect(self):
+    def mqttConnect(self):
         client = MQTTClient(self.client_id, self.mqttServer, port=1883, user=self.hassUsername, password=self.hassPassword, keepalive=3600)
-        client.set_callback(self.mqtt_subscription_callback)
+        client.set_callback(self.mqttSubscriptionCallback)
         client.connect()
         client.subscribe(self.subscriptionTopic)
         print(f'Connected to {self.mqttServer} MQTT Broker')
         print(f'Subscribed to topic: "{self.subscriptionTopic}"')
         return client
 
-    def setup_handlers(self):
-        self.switchDoorOpen.irq(trigger=Pin.IRQ_RISING, handler=self.door_open_handler)
-        self.switchDoorClosed.irq(trigger=Pin.IRQ_RISING, handler=self.door_closed_handler)
-        self.relayDoorMoving.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.door_moving_handler)
-        self.switchDoorObstructed.irq(trigger=Pin.IRQ_RISING, handler=self.door_obstructed_handler)
-        self.sensorPIR.irq(trigger=Pin.IRQ_RISING, handler=self.sensor_pir_handler)
+    def setupHandlers(self):
+        self.switchDoorOpen.irq(trigger=Pin.IRQ_RISING, handler=self.doorOpenHandler)
+        self.switchDoorClosed.irq(trigger=Pin.IRQ_RISING, handler=self.doorClosedHandler)
+        self.relayDoorMoving.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.doorMovingHandler)
+        self.switchDoorObstructed.irq(trigger=Pin.IRQ_RISING, handler=self.doorObstructedHandler)
+        self.sensorPIR.irq(trigger=Pin.IRQ_RISING, handler=self.sensorPirHandler)
 
-    def publish_bme_values(self):
+    def publishBmeValues(self):
         bme = bme280.BME280(i2c=self.i2c)
         self.client.publish("pipicow/bme280/temperature", bme.values[0])
         self.client.publish("pipicow/bme280/pressure", bme.values[1])
         self.client.publish("pipicow/bme280/humidity", bme.values[2])
 
-    def publish_door_state(self):
+    def publishDoorState(self):
         self.client.publish("pipicow/doorStateOpen", str(self.switchDoorOpen.value()))
         self.client.publish("pipicow/doorStateClosed", str(self.switchDoorClosed.value()))
         self.client.publish("pipicow/doorStateMoving", str(self.relayDoorMoving.value()))
         self.client.publish("pipicow/doorStateObstructed", str(self.switchDoorObstructed.value()))
 
-    def flash_leds(self):
+    def flashLeds(self):
         self.ledAlive.toggle()
         self.ledInternal.toggle()
         time.sleep(0.05)
         self.ledAlive.toggle()
         self.ledInternal.toggle()
 
-    def schedule_tasks(self):
-        schedule.every(5).seconds.do(self.flash_leds)
-        schedule.every(10).seconds.do(self.publish_bme_values)
-        schedule.every(60).seconds.do(self.publish_door_state)
+    def scheduleTasks(self):
+        schedule.every(5).seconds.do(self.flashLeds)
+        schedule.every(10).seconds.do(self.publishBmeValues)
+        schedule.every(60).seconds.do(self.publishDoorState)
 
     async def main(self):
         while True:
@@ -148,7 +148,7 @@ class RaspberryPiPicoW:
             schedule.run_pending()
             await asyncio.sleep(1)
 
-    def door_open_handler(self, pin):
+    def doorOpenHandler(self, pin):
         time.sleep_ms(100)
         self.doorStateClosed = False
         self.doorStateMoving = False
@@ -156,7 +156,7 @@ class RaspberryPiPicoW:
             self.doorStateOpen = True
             self.client.publish("pipicow/doorState", "open")
 
-    def door_closed_handler(self, pin):
+    def doorClosedHandler(self, pin):
         time.sleep_ms(100)
         self.doorStateOpen = False
         self.doorStateMoving = False
@@ -164,7 +164,7 @@ class RaspberryPiPicoW:
             self.doorStateClosed = True
             self.client.publish("pipicow/doorState", "closed")
 
-    def door_moving_handler(self, pin):
+    def doorMovingHandler(self, pin):
         time.sleep_ms(1000)
         self.doorStateOpen = False
         self.doorStateClosed = False
@@ -175,20 +175,20 @@ class RaspberryPiPicoW:
             self.doorStateMoving = False
             self.client.publish("pipicow/doorState", "stopped")
 
-    def door_obstructed_handler(self, pin):
+    def doorObstructedHandler(self, pin):
         time.sleep_ms(100)
         if not self.doorStateObstructed:
             self.doorStateObstructed = True
             self.client.publish("pipicow/doorState", "obstructed")
 
-    def sensor_pir_handler(self, pin):
+    def sensorPirHandler(self, pin):
         time.sleep_ms(100)
         if self.sensorPIR.value():
             self.client.publish("pipicow/pir", "motion")
 
 if __name__ == "__main__":
     # Create an instance of the RaspberryPiPicoW class and start main
-    pico_device = RaspberryPiPicoW()
-    pico_device.publish_bme_values()
-    pico_device.publish_door_state()
-    asyncio.run(pico_device.main())
+    picoDevice = RaspberryPiPicoW()
+    picoDevice.publishBmeValues()
+    picoDevice.publishDoorState()
+    asyncio.run(picoDevice.main())
